4d3f14e887eb483396b8fc4a726fe055e91c15d2
Igor-Ts <igor.tsepulkovski@gmail.com>
2022-03-15 18:42:24 +0200

    Added new folder static Created new method(CRUD) create in deviceController In method was realized img adding and movment into static folder with new name

--- server/index.js
+++ server/index.js
@@ -6,14 +6,17 @@ const cors = require('cors')
 const fileUpload = require('express-fileupload')
 const router = require('./routes/index')
 const errorHandler = require('./middleware/ErrorHandlingMiddleware')
+const path = require('path')
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
 app.use(cors())
 app.use(express.json()) // enable to parse json
+app.use(express.static(path.resolve(__dirname, 'static')))
 app.use(fileUpload({}))
 app.use('/api', router)
+
 // need to be last
 app.use(errorHandler)
 
