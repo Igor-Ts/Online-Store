9490397ca84c23ca1b3c0719e661aad4336c3259
Igor-Ts <igor.tsepulkovski@gmail.com>
2022-03-17 22:17:03 +0200

    Added new checkRoleMiddleware.js Added the role logic

---
+++ server/middleware/checkRoleMiddleware.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken')
+
+module.exports = function(role) {
+    return function (req, res, next) {
+        if (req.method === 'OPTIONS') {
+            next()
+        }
+        try {
+            const token = req.headers.authorization.split(' ')[1] // usually token have view like Bearer fdsfs
+            if (!token) {
+                return res.status(401).json({message: 'Welcome. You are unauthorised'})
+            }
+            const decoded = jwt.verify(token, process.env.SECRET_KEY)
+            if (decoded.role !== role) {
+                return res.status(403).json({message: 'access denied'})
+            }
+            req.user = decoded
+            next()
+        } catch (e) {
+            res.status(401).json({message: 'Welcome. You are unauthorised'})
+        }
+    }
+}
+
