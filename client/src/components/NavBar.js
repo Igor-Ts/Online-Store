1aaab1ebb3c69274258b877a5f85da88fc04c0ff
Igor-Ts <igor.tsepulkovski@gmail.com>
2022-03-23 22:06:59 +0200

    Created modal form to adding brand, device and type

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -4,12 +4,14 @@ import Navbar from "react-bootstrap/Navbar"
 import Nav from "react-bootstrap/Nav"
 import Container from "react-bootstrap/Container"
 import {NavLink} from "react-router-dom"
-import {SHOP_ROUTE} from "../utils/consts"
+import {ADMIN_ROUTE, LOGIN_ROUTE, SHOP_ROUTE} from "../utils/consts"
 import {Button} from "react-bootstrap"
 import {observer} from "mobx-react-lite"
+import {useNavigate} from "react-router-dom"
 
 const NavBar = observer(() => {
 	const {user} = useContext(Context)
+	const navigate = useNavigate()
 	console.log(user)
 	return (
 		<Navbar bg="dark" variant="dark">
@@ -19,8 +21,15 @@ const NavBar = observer(() => {
 				</NavLink>
 				{user.isAuth ? (
 					<Nav className="ml-auto" style={{color: "white"}}>
-						<Button variant={"outline-light"}>Admin</Button>
-						<Button variant={"outline-light"} className="ms-2">
+						<Button
+							variant={"outline-light"}
+							onClick={() => navigate(ADMIN_ROUTE)}>
+							Admin
+						</Button>
+						<Button
+							variant={"outline-light"}
+							onClick={() => navigate(LOGIN_ROUTE)}
+							className="ms-2">
 							Log out
 						</Button>
 					</Nav>
--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -1,7 +1,29 @@
 import React from "react"
+import {Button, Container} from "react-bootstrap"
+import CreateBrand from "../components/modals/CreateBrand"
+import CreateDevice from "../components/modals/CreateDevice"
+import CreateType from "../components/modals/CreateType"
 
 const Admin = () => {
-	return <div>Admin</div>
+	return (
+		<Container className="d-flex flex-column">
+			<Button variant={"outline-dark"} className="mt-4 p-2">
+				{" "}
+				Add type
+			</Button>
+			<Button variant={"outline-dark"} className="mt-4 p-2">
+				{" "}
+				Add brand
+			</Button>
+			<Button variant={"outline-dark"} className="mt-4 p-2">
+				{" "}
+				Add device
+			</Button>
+			<CreateBrand />
+			<CreateDevice />
+			<CreateType show={true} />
+		</Container>
+	)
 }
 
 export default Admin
