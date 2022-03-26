196bb52b05386d0693d6c1bc9dffa30063be59bb
Igor-Ts <igor.tsepulkovski@gmail.com>
2022-03-26 18:38:48 +0200

    Added a feature to create type to DB

--- client/src/components/modals/CreateType.js
+++ client/src/components/modals/CreateType.js
@@ -1,7 +1,15 @@
-import React from "react"
+import React, {useState} from "react"
 import {Button, Form, Modal} from "react-bootstrap"
+import {createType} from "../../http/deviceAPI"
 
 const CreateType = ({show, onHide}) => {
+	const [value, setValue] = useState("")
+
+	const addType = () => {
+		createType({name: value}).then((data) => setValue(""))
+		onHide()
+	}
+
 	return (
 		<Modal show={show} onHide={onHide} centered>
 			<Modal.Header closeButton>
@@ -12,6 +20,8 @@ const CreateType = ({show, onHide}) => {
 			<Modal.Body>
 				<Form>
 					<Form.Control
+						value={value}
+						onChange={(e) => setValue(e.target.value)}
 						placeholder={"Write a type name"}></Form.Control>
 				</Form>
 			</Modal.Body>
@@ -19,7 +29,7 @@ const CreateType = ({show, onHide}) => {
 				<Button variant="outline-danger" onClick={onHide}>
 					Close
 				</Button>
-				<Button variant="outline-success" onClick={onHide}>
+				<Button variant="outline-success" onClick={addType}>
 					Add
 				</Button>
 			</Modal.Footer>
