0bbd9a202458751cfd8754e7425cbcf441b5a05c
Igor-Ts <igor.tsepulkovski@gmail.com>
2022-03-26 18:49:19 +0200

    Added a feature to create brand into DB

--- client/src/components/modals/CreateBrand.js
+++ client/src/components/modals/CreateBrand.js
@@ -1,7 +1,14 @@
-import React from "react"
+import React, {useState} from "react"
 import {Button, Form, Modal} from "react-bootstrap"
+import {createBrand} from "../../http/deviceAPI"
 
 const CreateBrand = ({show, onHide}) => {
+	const [value, setValue] = useState("")
+
+	const addBrand = () => {
+		createBrand({name: value}).then((data) => setValue(""))
+		onHide()
+	}
 	return (
 		<Modal show={show} onHide={onHide} centered>
 			<Modal.Header closeButton>
@@ -12,14 +19,16 @@ const CreateBrand = ({show, onHide}) => {
 			<Modal.Body>
 				<Form>
 					<Form.Control
-						placeholder={"Write a type name"}></Form.Control>
+						value={value}
+						onChange={(e) => setValue(e.target.value)}
+						placeholder={"Write a brand name"}></Form.Control>
 				</Form>
 			</Modal.Body>
 			<Modal.Footer>
 				<Button variant="outline-danger" onClick={onHide}>
 					Close
 				</Button>
-				<Button variant="outline-success" onClick={onHide}>
+				<Button variant="outline-success" onClick={addBrand}>
 					Add
 				</Button>
 			</Modal.Footer>
